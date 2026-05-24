54541c153fc2c94d91f0c256856d6727d208c6fa
lgiraldo-dev <lgiraldo@quanspire.com>
2026-05-24 17:25:10 -0500

    Se agregan variables de Wompi en la config del back para test

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
     access_token_minutes: int = 60
     refresh_token_minutes: int = 10080
     frontend_origin: str = "http://localhost:5173,http://127.0.0.1:5173"
-    wompi_public_key: str
-    wompi_integrity_key: str
-    wompi_events_key: str
+    wompi_public_key: str = "pub_test_ezjBoCzrT4tpmfiUsEiqC2FS7tovdGQL"
+    wompi_integrity_key: str = "test_integrity_KeQ2kjKqYkkHTUUI8LfrWlxc7Nyu9lao"
+    wompi_events_key: str = "test_events_QY1UIt4KCzXr3BJiL6h0lKt7XHQAUVBq"
 
     @property
     def allowed_origins(self) -> list[str]:
